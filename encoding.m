6f9644ce908d396556d1958ad581808fbdecfce2
Supun <supun2110@gmail.com>
2021-03-27 00:22:03 +0530

    Fixed proper displaying of the digitized signal

--- encoding.m
+++ encoding.m
@@ -8,22 +8,30 @@ function e_vals = encoding(q_vals, q_levels, m, Ts)
        
        e_vals(i, :) = hold_bits;
    end
+ 
+   step = 10^(-3);
    
-   Ts_graph = ones(1, size(q_vals,2)*num_bits);
-   e_vals_graph = ones(1, size(q_vals, 2)*num_bits);
+   Ts_graph = 1:step:size(q_vals,2)*num_bits+1;
+   e_vals_graph = 1:step:size(q_vals, 2)*num_bits+1;
    
-   pos = 1;
+   Ts_graph(end) = [];
+   e_vals_graph(end) = [];
    
-   bias = Ts(2) / (2 * num_bits);
+   index_count = 0;
    
-   for i = 1:size(Ts, 2)
-       Ts_graph(1, pos:pos+num_bits-1) = linspace(Ts(i) + bias, Ts(i)+Ts(2)-bias, num_bits);
-       e_vals_graph(1, pos:pos+num_bits-1) = e_vals(i,:);
-       pos = pos + num_bits;
+   for i = 1:size(e_vals, 1)
+       for j = 1:size(e_vals, 2)
+           for z = 1:(1/step)
+               e_vals_graph(index_count+z) = e_vals(i,j); 
+           end
+           index_count = index_count + (1/step);
+       end 
    end
    
    figure;
    
-   stem(Ts_graph, e_vals_graph);
+   plot(Ts_graph, e_vals_graph);
+   ylim([-0.5, 1.5]);
+   grid;
    
 end
